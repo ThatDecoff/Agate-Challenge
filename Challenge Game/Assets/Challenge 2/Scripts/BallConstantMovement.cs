545a10538612d4126632d8c20c5819b0e660aaed
Adrian Kaiser <adriankaiser.rian@gmail.com>
2021-11-14 16:24:02 +0700

    Added constant ball movement

---
+++ Challenge Game/Assets/Challenge 2/Scripts/BallConstantMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallConstantMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        SetMovement();
+    }
+
+    private void SetMovement()
+    {
+        float speed = Random.Range(100f, 500f);
+        Vector2 force = new Vector2(Random.value, Random.value);
+
+        rb2d.AddForce(force * speed);
+    }
+}
