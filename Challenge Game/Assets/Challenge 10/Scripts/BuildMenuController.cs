1fa06b3d1dc394df4cadb51f98116cbff4fe3ee6
Adrian Kaiser <adriankaiser.rian@gmail.com>
2021-11-20 13:30:14 +0700

    Added build menu

---
+++ Challenge Game/Assets/Challenge 10/Scripts/BuildMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BuildMenuController : MonoBehaviour
+{
+    public Button ExitButton;
+
+    public List<MenuButton> builds = new List<MenuButton>();
+
+    private void Start()
+    {
+        foreach(MenuButton menu in builds)
+        {
+            SetMenuButton(menu.BuildButton, menu.BuildSceneName);
+        }
+
+        ExitButton.onClick.RemoveAllListeners();
+        ExitButton.onClick.AddListener(Application.Quit);
+    }
+
+    private void SetMenuButton(Button button, string Name)
+    {
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(() => { SceneManager.LoadScene(Name, LoadSceneMode.Single); });
+    }
+}
+
+[System.Serializable]
+public class MenuButton
+{
+    public Button BuildButton;
+    public string BuildSceneName;
+}
---
+++ Challenge Game/Assets/Challenge 10/Scripts/PersistentController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersistentController : MonoBehaviour
+{
+    private static PersistentController Instance = null;
+
+    public KeyCode BackKey = KeyCode.Tilde;
+
+    private void Start()
+    {
+        if(Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(BackKey))
+        {
+            SceneManager.LoadScene("Challenge10", LoadSceneMode.Single);
+        }
+    }
+}
